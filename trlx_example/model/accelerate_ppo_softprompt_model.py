c47362ef59b991ed1b5292cd8f153ba84810ca28
Andrew Dai <adai@tcd.ie>
2022-10-28 23:42:44 +0200

    softprompt prefix padding handling, whole model freezing, cleanup

--- trlx_example/model/accelerate_ppo_softprompt_model.py
+++ trlx_example/model/accelerate_ppo_softprompt_model.py
@@ -44,6 +44,7 @@ class SoftEmbedding(nn.Module):
         super().__init__()
         self.wte = wte
         self.n_tokens = n_tokens
+        self.padding_token_id = 50256 # used when input tensors are prefix padded
         self.learned_embedding = nn.parameter.Parameter(
             self.initialize_embedding(
                 wte, n_tokens, random_range, initialize_from_vocab
@@ -76,9 +77,24 @@ class SoftEmbedding(nn.Module):
         Returns:
             torch.float: encoding of text concatenated with learned task specifc embedding
         """
-        input_embedding = self.wte(tokens[:, self.n_tokens :])
-        learned_embedding = self.learned_embedding.repeat(input_embedding.size(0), 1, 1)
-        return torch.cat([learned_embedding, input_embedding], 1)
+        prompt_tokens = tokens[:, self.n_tokens :] # without extra soft prompt padding
+        if self.padding_token_id in prompt_tokens[:, 0]: # padding is applied as prefix
+            seq_embedding = self.wte(tokens)
+            padding_tensor = torch.tensor([self.padding_token_id]).to(seq_embedding.device)
+            
+            # index in each sequence in tokens just after last prefix padding
+            # this would be where the (first) soft prompt embedding should be set
+            first_prompt_indices = (prompt_tokens == padding_tensor).int().argmin(axis=1)
+            
+            # replace embeddings at soft prompt indices with correct soft embeddings
+            for batch_idx, first_prompt_idx in enumerate(first_prompt_indices.tolist()):
+                seq_embedding[batch_idx, first_prompt_idx:first_prompt_idx+self.n_tokens] = self.learned_embedding
+
+            return seq_embedding
+        else:
+            input_embedding = self.wte(prompt_tokens)
+            learned_embedding = self.learned_embedding.repeat(input_embedding.size(0), 1, 1)
+            return torch.cat([learned_embedding, input_embedding], 1)
 
 
 @register_model
@@ -89,6 +105,8 @@ class AcceleratePPOSoftpromptModel(AcceleratePPOModel):
         assert (
             config.method.n_soft_tokens > 0
         ), "Number of soft prompt tokens should be >=1"
+
+        self.soft_dummy_token_id = 50256  # dummy token for padding soft prompts
 
         # account for extra prefix tokens
         self.config.method.gen_kwargs["max_length"] += self.n_soft_tokens
@@ -104,7 +122,6 @@ class AcceleratePPOSoftpromptModel(AcceleratePPOModel):
         # and the model's input embeddings.
         # the model will always concatenate learned softprompt embeddings as prefix to the prompt/query after it's set
         # use config option to initialize embedding from existing vocab, or random
-        self.soft_dummy_token_id = 50256  # dummy token for padding soft prompts
         self.n_soft_tokens = (
             config.method.n_soft_tokens
         )  # number of prefix tokens added to prompt, with learned embeddings
@@ -117,64 +134,44 @@ class AcceleratePPOSoftpromptModel(AcceleratePPOModel):
 
         model.gpt.set_input_embeddings(s_wte)
 
+        # if all layers are frozen, freeze other non-learned-embedding params in addition to layer params
+        if self.config.model.num_layers_unfrozen == 0:
+            transformer_layers = model.gpt.transformer
+            transformer_layers.wte.requires_grad_(False)
+            transformer_layers.wpe.requires_grad_(False)
+
         return model
     
-    def act(
-        self, data: PromptBatch
-    ) -> Tuple[
-        TensorType["chunk_size", "input_length"],
-        TensorType["chunk_size", "gen_size"],
-        Iterable[str],
-    ]:
-        # TODO: align recent trlx merge changes with example rollout actions
-        data.tokens = torch.cat(
+    def generate(self, input_ids, attention_mask=None, **kwargs):
+        """Wraps hf's `generate` adding some specific method's defaults"""
+        # pad for soft prompts (using same token as for padding)
+        input_ids = torch.cat(
             [
                 torch.full(
-                    (data.tokens.shape[0], self.n_soft_tokens), self.soft_dummy_token_id
-                ),
-                data.tokens,
+                    (input_ids.shape[0], self.n_soft_tokens), self.soft_dummy_token_id
+                ).to(input_ids.device),
+                input_ids,
             ],
             1,
         )
-        query_tensors = data.tokens.to(
-            self.accelerator.device
-        )  # [B, N] #TODO(dahoas): This may need to be changed
-        with torch.no_grad():
-            # TODO(dahoas): swap this out for custom generate to if this fixes issue
-
-            # need to pad attention_mask and input_ids to be full seq_len + n_learned_tokens
-            # even though it does not matter what you pad input_ids with, it's just to make HF happy
-            batch_dim = self.dummy_input.shape[0]
-            self.dummy_input = torch.cat(
+        input_ids = input_ids.to(self.accelerator.device)
+        if attention_mask is not None:
+            # extend for soft prompts (by extending mask at the end of tensor)
+            attention_mask = torch.cat(
                 [
+                    attention_mask,
                     torch.full(
-                        (batch_dim, self.n_soft_tokens), self.soft_dummy_token_id
-                    ),
-                    self.dummy_input,
+                        (attention_mask.shape[0], self.n_soft_tokens), 1
+                    ).to(attention_mask.device),
                 ],
                 1,
             )
-            _ = self.model(
-                self.dummy_input.to(self.accelerator.device)
-            )  # Dummy pass to make things play nice with accelerate
-            # Removed synced gpus
-            attn_mask = torch.full(
-                (query_tensors.shape[0], query_tensors.shape[1]), 1
-            ).to(self.accelerator.device)
-            response = self.model.generate(
-                query_tensors,
-                pad_token_id=self.tokenizer.eos_token_id,
-                attention_mask=attn_mask,
-                use_cache=False,  # needed for softprompt compatibility
-                **self.config.method.gen_kwargs
-            )
-            response_tensors = response[
-                :,
-                query_tensors.size()[1] : query_tensors.size()[1]
-                + self.config.train.gen_size,
-            ]
-            query_tensors = query_tensors[
-                :, self.n_soft_tokens :
-            ]  # remove softprompt padding tokens
-        response_text = self.tokenizer.batch_decode(response_tensors)
-        return query_tensors, response_tensors, response_text
+            attention_mask = attention_mask.to(self.accelerator.device)
+
+        kwargs = dict(self.generate_kwargs, **kwargs)
+
+        with torch.no_grad():
+            return self.accelerator.unwrap_model(self.model).generate(
+                input_ids=input_ids, attention_mask=attention_mask, use_cache=False, **kwargs
+            ) # disable cache needed for softprompt compatibility
+
