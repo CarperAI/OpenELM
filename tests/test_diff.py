f860e789eb47478541d375f5c6b231c44e6b4691
Honglu Fan <honglu2875@gmail.com>
2022-12-12 13:52:36 -0500

    Modified benchmark config; Added diff benchmark script; Completed verify_diff and added tests.

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,4 +1,6 @@
-from elm.util.diff_eval import apply_diff, parse_line_info, replace_text, split_diff
+import itertools
+
+from elm.util.diff_eval import apply_diff, parse_line_info, replace_text, split_diff, verify_diff
 
 
 def test_diff():
@@ -28,20 +30,119 @@ def test_diff():
         "hanxi/patch-2\n\nfix a typo\n<DFF> @@ -30,7 +30,7 @@ http {\n         # Error log\n         error_log "
         "logs/{{LOR_ENV}}-error.log;\n \n-        # this variable is for view render（lua-resty-template)\n+        "
         "# this variable is for view render(lua-resty-template)\n         set $template_root '';\n \n         "
-        "location /static {\n "
+        "location /static {\n"
     )
     result = split_diff(test)
     # Because it is a valid diff, whichever method we apply the patch should result in the same.
     assert apply_diff(
         result["file"], result["diff"], use_line_number=False
     ) == apply_diff(result["file"], result["diff"], use_line_number=True)
+    # Reject if pre-diff lines don't fully match
+    assert apply_diff(result["file"], result["diff"] + " ", use_line_number=True) == result["file"]
+
+    # Test ADDFILE
+    test = (
+        "<NME> test.py\n<BEF> ADDFILE\n<MSG> asldkjf\n<DFF> @@ -1,0 +0,0 @@\n"
+        "+aaaaaaa\n+bbbbbbb"
+    )
+    result = split_diff(test)
+    assert apply_diff(result["file"], result["diff"], use_line_number=False) == "aaaaaaa\nbbbbbbb"
+    assert apply_diff(result["file"], result["diff"], use_line_number=True) == ""
+
+    # Test the loose patching (still performs the patching until invalid diff lines)
+    test = (
+        "<NME> test.py\n<BEF> cccccc\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\ninvalid\n"
+    )
+    result = split_diff(test)
+    assert apply_diff(result["file"], result["diff"], use_line_number=False) == "aaaaaaa\nbbbbbbb\ncccccc"
+    # the first part -1,1 is consistent, so perform the patching
+    assert apply_diff(result["file"], result["diff"], use_line_number=True) == "aaaaaaa\nbbbbbbb\ncccccc"
+
+    # Test multiple @@ ... @@
+    test = (
+        "<NME> test.py\n<BEF> cccccc\ndddddd\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\n@@ -2,1 +4,1 @@\n-dddddd\n+eeeeee\n"
+    )
+    result = split_diff(test)
+    assert apply_diff(result["file"], result["diff"], use_line_number=False) == "aaaaaaa\nbbbbbbb\ncccccc\neeeeee"
+    assert apply_diff(result["file"], result["diff"], use_line_number=True) == "aaaaaaa\nbbbbbbb\ncccccc\neeeeee"
 
 
 def test_replace_text():
     test = "abcdawasdfwgvwavwefadwwasd"
     before = "wasd"
     after = "1234"
-    assert replace_text(test, before, after) == "abcda1234fwgvwavwefadwwasd"
+    assert replace_text(test, before, after, 0) == ("abcda1234fwgvwavwefadwwasd", 9)
 
     test = "asdsdsadafwasd"
-    assert replace_text(test, before, after) == "asdsdsadaf1234"
+    assert replace_text(test, before, after, 0) == ("asdsdsadaf1234", 14)
+
+
+def test_verify_diff():
+    """
+    Go over as many bad diff as possible to test the status code of `verify_diff`
+    """
+
+    # Valid diff
+    test = (
+        "<NME> test.py\n<BEF> cccccc\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\n"
+    )
+    assert verify_diff(test) == 0
+    # Multiple valid
+    test = (
+        "<NME> test.py\n<BEF> cccccc\ndddddd\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\n@@ -2,1 +4,1 @@\n-dddddd\n+eeeeee\n"
+    )
+    assert verify_diff(test) == 0
+
+    # Test ADDFILE
+    test = (
+        "<NME> test.py\n<BEF> ADDFILE\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\n"
+    )
+    assert verify_diff(test) == 6  # Wrong line numbers
+    test = (
+        "<NME> test.py\n<BEF> ADDFILE\n<MSG> asldkjf\n<DFF> @@ -0,0 +0,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\n"
+    )
+    assert verify_diff(test) == 6  # Contains lines not starting with "+"
+    test = (
+        "<NME> test.py\n<BEF> ADDFILE\n<MSG> asldkjf\n<DFF> @@ -0,0 +0,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n+cccccc\n"
+    )
+    assert verify_diff(test) == 0
+
+    test = (
+        "<NME> test.py\n<BEF> cccccc\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n ccccc\n"
+    )
+    assert verify_diff(test) == 1
+    for a, b, c, d in itertools.product([0, 2], repeat=4):
+        for e in ["c", ""]:
+            test = (
+                f"<NME> test.py\n<BEF> cccccc\n<MSG> asldkjf\n<DFF> @@ -{a},{b} +{c},{d} @@\n"
+                f"+aaaaaaa\n+bbbbbbb\n ccccc{e}\n"
+            )
+            assert verify_diff(test) == 2 if e == "c" else 3
+    test = (
+        "<NME> test.py\n<BFE> cccccc\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,4 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\n"
+    )
+    assert verify_diff(test) == 4  # Invalid format (BFE instead of BEF)
+    test = (
+        "<NME> test.py\n<BEF> cccccc\n<MSG> asldkjf\n<DFF> @@ -1,1 +1,3 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n*cccccc\n"
+    )
+    assert verify_diff(test) == 5  # Invalid hunk (start with other char)
+    test = (
+        "<NME> test.py\n<BEF> cccccc\n<MSG> asldkjf\n<DFF> @@ +1,1 -1,4 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n cccccc\n"
+    )
+    assert verify_diff(test) == 6  # Invalid format (wrong format in @@ ... @@)
+    test = (
+        "<NME> test.py\n<BEF> cccccc\n<MSG> asldkjf\n<DFF> @@ +1,1 -1,4 @@\n"
+        "+aaaaaaa\n+bbbbbbb\n*cccccc\n"
+    )
+    assert verify_diff(test) == 7  # Both
