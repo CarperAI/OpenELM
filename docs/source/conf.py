0fe38cdc3802a78827cda970d6c28ca8874cb056
Herbie Bradley <mail@herbiebradley.com>
2022-12-24 17:04:16 +0000

    Update conf.py version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "OpenELM"
 copyright = "2022, CarperAI"
 author = "CarperAI"
-release = "0.1.0"
+release = "0.1.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
