3cf1664661869e207e4fe4f5e2f4781135adc7a9
Herbie Bradley <mail@herbiebradley.com>
2022-12-25 13:19:29 +0000

    Edit sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,8 +71,6 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
 html_theme_options = {
     "logo_only": True,
     "display_version": True,
