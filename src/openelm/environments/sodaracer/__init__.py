d0bf7bdac2fc1466788ca00b284edd3406f21e1c
Herbie Bradley <mail@herbiebradley.com>
2023-02-03 18:50:56 +0000

    fix init imports

--- src/openelm/environments/sodaracer/__init__.py
+++ src/openelm/environments/sodaracer/__init__.py
@@ -1,20 +1,4 @@
-from openelm.environments.sodaracer.helpers import (
-    Bone,
-    DistanceAccessor,
-    Entity,
-    EntityType,
-    Muscle,
-)
 from openelm.environments.sodaracer.simulator import IESoRWorld, SodaraceSimulator
 from openelm.environments.sodaracer.walker import Walker
 
-__all__ = [
-    "Bone",
-    "DistanceAccessor",
-    "Entity",
-    "EntityType",
-    "Muscle",
-    "IESoRWorld",
-    "SodaraceSimulator",
-    "Walker",
-]
+__all__ = ["IESoRWorld", "SodaraceSimulator", "Walker"]
