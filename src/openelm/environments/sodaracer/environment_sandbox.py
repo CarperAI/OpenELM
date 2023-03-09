199908d6ac53c6ff1c66fbfcf159ca0405cc1967
Andrew Dai <adai@tcd.ie>
2023-03-09 23:18:00 +0100

    clear up naming in sandbox script
    Co-authored-by: Marco <marcobellagente93@users.noreply.github.com>

--- src/openelm/environments/sodaracer/environment_sandbox.py
+++ src/openelm/environments/sodaracer/environment_sandbox.py
@@ -2,7 +2,7 @@ from openelm.environments.sodaracer.box2d_examples.framework import main
 
 from openelm.environments.sodaracer.simulator import IESoRWorld
 from openelm.environments.sodaracer.walker.radial import (
-    make_walker as make_walker_runner,
+    make_walker as make_walker_radial,
 )
 
 
@@ -11,7 +11,7 @@ class DemoLoadWalkerWorld(IESoRWorld):
     def __init__(self, canvas_size: tuple[int, int] = (150, 200)):
         super().__init__(canvas_size)
         # here we import an example make_walker method, and load it to world
-        self.load_body_into_world(make_walker_runner().to_dict())
+        self.load_body_into_world(make_walker_radial().to_dict())
 
 
 if __name__ == "__main__":
